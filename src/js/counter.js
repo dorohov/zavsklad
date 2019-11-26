06248f2c0c1eb7cde855b1ea540d462835366a51
Pavel Proyavin <pavel@dorohovdesign.com>
2019-11-26 17:48:04 +0300

    cl f 261119

---
+++ src/js/counter.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict"
+    $(function() {
+
+        $('.counter button').on('click', function() {
+            var thisInput = $(this).siblings('input')
+            var thisValue = thisInput.val()
+
+            if($(this).hasClass('is--plus') && thisValue < 9999) thisValue++;
+            else if($(this).hasClass('is--minus') && thisValue > 1) thisValue--;
+
+            if(thisValue < 1 || thisValue > 9999) thisValue = 0
+
+            thisInput.val(thisValue)
+        })
+
+    })
+})(jQuery);
---
+++ src/js/modals.js
@@ -0,0 +1,26 @@
+function openModal(modalID) {
+    $(modalID).modal({
+        fadeDuration: 100,
+        showClose: false,
+    })
+}
+
+(function($) {
+    "use strict"
+    $(function() {
+
+        $.modal.fadeDuration = 100
+        $.modal.showClose = false
+
+        $('a.modal-open').on('click', function(e) {
+            e.preventDefault()
+            var thisModalId = $(this).attr('href')
+            openModal(thisModalId)
+            return false;
+        })
+
+        // openModal('#modal_form')
+        openModal('#modal_success')
+
+    })
+})(jQuery);
